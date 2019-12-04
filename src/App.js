d73c69accec554ff33efccfffb782478f6017c18
YujeongShin570 <tls980704@gmail.com>
2019-12-04 10:50:36 +0900

    yujeong update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, {Component} from "react";
 import {Route,BrowserRouter} from 'react-router-dom'
 import SignIn from './SignIn.js'
 import Main from './Main.js'
-import SignUp from './companysignup.js'
+import CompanySignUp from './companysignup.js'
+import PersonalSignUp from './personalsignup.js'
 //test
 class App extends Component {
   render () {
@@ -11,7 +12,8 @@ class App extends Component {
     <div>
       <Route exact path='/' component={SignIn}/>
       <Route exact path='/main' component={Main}/>
-      <Route exact path='/companysignup' component={SignUp}/>
+      <Route exact path='/companysignup' component={CompanySignUp}/>
+      <Route exact path='/personalsignup' component={PersonalSignUp}/>
     </div>
     </BrowserRouter>
     )
