5d362f0cdce602eff3f4eeb8f16c80ffcae378fd
Kyuri-Kyeong <vinca1022@gmail.com>
2019-12-09 05:12:16 +0900

    Kyuri: mypage change

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -4,6 +4,7 @@ import {NavigationBar} from './Main.js'
 import {connect} from 'react-redux'
 import {mapStateToProps} from 'react-redux-firebase'
 import axios from 'axios'
+import Idea from './components/Idea'
 
 class MyPage extends Component{
     state ={
@@ -37,7 +38,64 @@ class MyPage extends Component{
                 { userType == 1 ? 
                     <Link to="./mypage/writeidea"><div id="writeidea">+</div></Link> 
                     : <Link to="./mypage/writecompa"><div id="writecompa">+</div></Link> }
+                <DisplayMyIdeas/>
             </Fragment>
+        )
+    }
+}
+class DisplayMyIdeas extends Component {
+    state = {
+        myIdeas: null,
+        userName: ""
+    }
+    componentDidMount() {
+        var value = localStorage.getItem('FBIdToken');
+        let axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                "Access-Control-Allow-Origin": "*",
+                "Autorizaion": value
+            }
+          };
+        axios.get('/user', axiosConfig)
+        .then(res => 
+            this.setState({
+                userName: res.data.userName
+            })
+        ).catch(err => {
+            console.log(err);
+        })
+        const {userName} = this.state;
+        const {string} = '/user' + userName+'/idea';
+        axios.get(string)
+        .then(myIdeas=> {
+            console.log("in axios myIdeas");
+            console.log(myIdeas.data);
+            this.setState({
+                myIdeas: myIdeas.data
+            })}
+        )
+        .catch(err => console.log(err));
+
+    }
+    render(){
+        const {myIdeas} = this.state;
+        return(
+            <div class="posts">
+                { myIdeas && myIdeas.map(idea => {
+                    return(
+                        <Idea title= {idea.title}
+                        body= {idea.body}
+                        category= {idea.category}
+                        author= {idea.author}
+                        dueDate= {idea.dueDate}
+                        userName= {idea.userName}
+                        userImage= {idea.imageUrl}
+                        createdAt= {idea.createdAt}/>
+                        )
+                    })
+                }
+            </div>
         )
     }
 }
