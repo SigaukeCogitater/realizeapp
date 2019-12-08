8d90aa13e4e3aa315e2be3b8b343f02606c21e7d
PyungkangHong <53274042+PyungkangHong094@users.noreply.github.com>
2019-12-09 08:55:34 +0900

    update
    update

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,50 +1,187 @@
-import React, {Component} from "react";
-import {Route, Link} from 'react-router-dom'
-import Main from './Main.js'
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+import AppIcon from '../images/icon.png';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+//MUI Stuff
 
-class SignIn extends Component {
-    state={
-        ID: "",
-        HidePassword: "",
-        Password: ""
-    }
-    inputID = (e) => {
-        this.setState({
-            ID: e.target.value
-        });
-    }
-    inputPassword = (e) => {
-        this.setState({
-            Password: e.target.value
-        });
-        
-    }
-    goTo = () => {
-        
-    }
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-    empty= (e) =>{}
+
+//Jquery
+
+// import $ from 'jquery';
+
+const styles = {
+    form: {
+        textAlign: 'center'
+    },
+    image: {
+        margin: '20px auto 20px auto'
+    },
+    pageTitle:{
+        margin: '10px auto 10px auto'
+    },
+    TextField:{
+        margin: '10px auto 10px auto'
+    },
+    button:{
+        marginTop: 20 
+    },
+    customError: {
+        color: 'red',
+        fontSize: '0.8rem',
+        marginTop: 10
+    },
+    progress:{
+        position: 'absolute'
+    },
+    small:{
+        marginTop: 10
+    }
+    
+};
+
+class signin extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            email: '',
+            password: '',
+            loading: false,
+            errors:{}
+            
+        };
+    };
+
+    handleSubmit = (event) => {
+        
+        console.log("submitted");
+        event.preventDefault();
+        this.setState({
+            loading: true
+        });
+        let axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                "Access-Control-Allow-Origin": "*",
+            }
+          };
+        
+          const userData ={
+            
+            "email": this.state.email,
+            "password": this.state.password
+        };
+          axios.post('/login', userData, axiosConfig)
+            .then(res => {
+                localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
+                this.setState({token : res.data.token,
+                loading: false});
+
+                this.props.history.push('/');
+                console.log(this.state);
+            })
+            .catch((err) => {
+                this.state.errors = err.response.data;
+                //this.state.errors.password = err.response.data.errors.password;
+                //this.state.errors.email = err.response.data.errors.email;
+                this.state.loading = false;
+                console.log(this.state.errors);
+
+                // this.setState({
+                //     errors: err.response.data,
+                //     loading: false
+                // });
+            });
+    };
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value //this will hold name and value of email or name depending on the event
+        });
+    };
+
     render() {
+        const { classes } = this.props;
+        const { errors, loading } = this.state;
         return(
-            <form>
-                <div id="SignIn">
-                    <h1 id = "font_set">Sign In</h1>
-                    <input id= "ID" placeholder="ID" onChange={this.inputID}></input>
-                    <br></br>
-                    <input type="password" id="Password" placeholder="Password" onChange={this.inputPassword}></input>
-                    <div class="button" id = "font_set" onClick={this.empty/*check if there's corresponding data in database*/}>Log In</div>
-                    {console.log("Password change:" + this.state.Password)}
-                    {console.log("ID change: " + this.state.ID)}
-                </div>
-                <div id="SignUp">
-                    <h1 id = "font_set">Sign Up</h1>
-                    <a href="/personalsignup"><div class="button" id = "font_set">Personal</div></a>
-                    <a href="/companysignup"><div class="button" id = "font_set">Company</div></a>
-                </div>
-            </form>
-        )
-    }
+            <Grid container className={classes.form}>
+                <Grid item sm/>
+                <Grid item sm>
+                    <img src={AppIcon} alt="RealiseApp icon" className={classes.image}/>
+                    <Typography variant="h3" className={classes.pageTitle}>
+                        Sign In
+                    </Typography>
+                    <form noValidate onSubmit={this.handleSubmit}>
+
+                        <TextField id="email" 
+                            name="email" 
+                            type="email" 
+                            label="Email" 
+                            className={classes.TextField}
+                            helperText={errors.email}
+                            error={errors.email ? true : false}
+                            value={this.state.email} 
+                            onChange={this.handleChange} 
+                            fullWidth/>
+                        <TextField id="password" 
+                            name="password" 
+                            type="password" 
+                            label="Password"
+                            helperText={errors.password}
+                            error={errors.password ? true : false} 
+                            className={classes.TextField}
+                            value={this.state.password} 
+                            onChange={this.handleChange} 
+                            fullWidth/>
+                        
+                            {errors.general && (
+                              <Typography variant="body2" className={classes.customError}>
+                                {errors.general}
+                              </Typography>
+                            )}
+
+                            <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            className={classes.button}
+                            onClick={this.handleSubmit}
+                            // disabled={loading}
+                            >
+                              Signin
+                              {loading && (
+                                <CircularProgress size={30} className={classes.progress} />
+                              )}
+                            </Button>  <br /><br /> 
+                            <small className={classes.small}>
+                                  dont have an account ? <br /> 
+                                  sign up a <Link to="/companysignup">company account</Link>
+                                  <br />
+                                  sign up a <Link to="/personalsignup">Personal account</Link>
+                            </small>                         
+
+                    </form>
+
+                </Grid>
+                <Grid item sm/>
+            </Grid>
+        );
+    };
+
+};
+
+signin.propTypes = {
+
+    classes: PropTypes.object.isRequired
+
 }
 
-
-export default SignIn;
+export default withStyles(styles)(signin);
